22ba72f5ce9984be362353cd44d9c90215f94fab
PotatoS229 <maksalievartemiy@gmail.com>
2026-02-03 14:56:49 +0300

    Новая статья по Rust

---
+++ RUST/Программы_по_дням/DAY_2/project_1/target/debug/deps/project_1-473464000011faee.d
@@ -0,0 +1,5 @@
+/home/noname/netStalker/RUST/Программы_по_дням/DAY_2/project_1/target/debug/deps/project_1-473464000011faee.d: src/main.rs
+
+/home/noname/netStalker/RUST/Программы_по_дням/DAY_2/project_1/target/debug/deps/project_1-473464000011faee: src/main.rs
+
+src/main.rs:
---
+++ RUST/Программы_по_дням/DAY_2/project_1/target/debug/project_1.d
@@ -0,0 +1,1 @@
+/home/noname/netStalker/RUST/Программы_по_дням/DAY_2/project_1/target/debug/project_1: /home/noname/netStalker/RUST/Программы_по_дням/DAY_2/project_1/src/main.rs
